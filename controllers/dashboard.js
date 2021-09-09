eb7d3750423a349529689e42eb386ac17511e63d
Glitch (navy-scintillating-oviraptor) <none>
2021-09-09 22:14:05 +0000

    🐢🎈 Updated with Glitch

---
+++ controllers/dashboard.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const axios = require("axios");
+const logger = require("../utils/logger");
+
+const dashboard = {
+  index(request, response) {
+    logger.info("dashboard rendering");
+    const viewData = {
+      title: "Template 1 Dashboard"
+    };
+    response.render("dashboard", viewData);
+  },
+
+  async addreport(request, response) {
+    logger.info("rendering new report");
+    let report = {};
+    const lat = request.body.lat;
+    const lng = request.body.lng;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=2bc4cb07cba2bb89cb7320f0f459b280`;
+    const result = await axios.get(requestUrl);
+    if (result.status == 200) {
+      const reading = result.data.current;
+      report.code = reading.weather[0].id;
+      report.temperature = reading.temp;
+      report.windSpeed = reading.wind_speed;
+      report.pressure = reading.pressure;
+      report.windDirection = reading.wind_deg;
+    }
+    console.log(report);
+    const viewData = {
+      title: "Weather Report",
+      reading: report
+    };
+    response.render("dashboard", viewData);
+  }
+};
+
+module.exports = dashboard;
